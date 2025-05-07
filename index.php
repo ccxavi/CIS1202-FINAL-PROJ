cd5080b880f79f4ce87be08dff021e4c2911825d
Czachary Xavier Villarin <ccxavillarin@gmail.com>
2025-05-07 11:27:43 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>_FINAL PROJ</title>
+    <link rel="stylesheet" href="./assets/css/global.css">
+    <link rel="stylesheet" href="./assets/css/header.css">
+    <link rel="stylesheet" href="./assets/css/main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" integrity="sha384-tViUnnbYAV00FLIhhi3v/dWt3Jxw4gZQcNoSCxCIFNJVCx7/D55/wXsrNIRANwdD" crossorigin="anonymous"></head>
+<body>
+    <header>
+        <div class="navbar">
+            <div class="logo">
+                <img src="" alt="logo">
+            </div>
+            <div class="links">
+                <a href="#"><div class="home">Home</div></a>
+                <a href="./views/explore.php"><div class="explore">Explore</div></a>
+                <a href="./views/guide.php"><div class="guide">Guide</div></a>
+                <a href="./views/collection.php"><div class="collection">Collection</div></a>
+                <a href="./views/community.php"><div class="community">Community</div></a>
+            </div>
+        </div>
+        <div class="auth">
+            <a href="#">Login/Register</a>
+            <img src="./assets/img/test.png" alt="pfp"></i>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <h1>Research Without the Doubt.</h1>
+            <div class="search">
+                <form action="POST">
+                    <input type="text" placeholder="Search" required>
+                    <button><i class="bi bi-search"></i></button>
+                </form>
+            </div>
+            <h5>Trending Topics, All Verified.</h5>
+        </div>
+    </main>
+    <footer>
+        <p>2025 All Rights Reserved</p>
+
+    </footer>
+</body>
+</html>
